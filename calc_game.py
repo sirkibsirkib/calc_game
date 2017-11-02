42184f79aeb26282979619a08c72c62a1b03528b
sirkibsirkib <christopher.esterhuyse@gmail.com>
2017-11-02 12:06:37 +0100

    nice

--- calc_game.py
+++ calc_game.py
@@ -1,15 +1,4 @@
-
-class Operation:
-    def __init__(self, printable, func):
-        self.printable = printable;
-        self.func = func
-
-    def apply_to(self, x):
-        return self.func(x)
-
-    def __repr__(self):
-        return self.printable
-
+from operations import *
 
 # class stores dict of solutions
 # solution -> (depth, op_sequence)
@@ -25,10 +14,11 @@ class Findings:
     def update_solution(self, number, depth, solution_sequence):
         self.solutions[number] = (depth, solution_sequence)
 
-    def __repr__(self):
+    def print_sols_with_min(self, min_steps, solution_predicate):
         s = ''
         for (k, v) in self.solutions.items():
-            s += '\t' + str(v[1]) + ' => ' + str(k) + '\n'
+            if v[0] >= min_steps and solution_predicate(k):
+                s += '\t' + str(v[1]) + ' => ' + str(k) + '\n'
         return s
 
 
@@ -36,33 +26,42 @@ sentinel = object()
 def derive(start_from_number, operations, max_depth, findings, depth_so_far = 1,
             operation_sequence = [], current_number=sentinel):
     if current_number == sentinel:
-        # this is stupid
+        # cant use another arg as default value for arg. need to use this sentinel nonsense
         current_number = start_from_number
 
     if depth_so_far > max_depth:
+        # stop condition
         return
 
     for o in operations:
+        # apply all possible args
         next_num = o.apply_to(current_number)
         found_in_depth = findings.depth_of_solution(next_num)
         next_seq = operation_sequence + [o]
+
         if found_in_depth == -1 or found_in_depth > depth_so_far:
             # update this solution
-            findings.update_solution(next_num, depth_so_far, next_seq   )
+            findings.update_solution(next_num, depth_so_far, next_seq)
 
         derive(start_from_number, operations, max_depth, findings, depth_so_far=depth_so_far+1,
                 operation_sequence = next_seq, current_number=next_num)
 
+
+
+# USER INPUT BEGIN
+start_num = 14
+min_steps = 5
+max_steps = 5
+solution_predicate = lambda x : x==12
+
 ops = [
-    Operation('+5', lambda x : x+5),
-    Operation('-2', lambda x : x-2),
-    Operation('/2', lambda x : x/2),
-    Operation('<0>', lambda x : x*10),
+    o_insert(6),
+    o_plus(5),
+    o_div(8),
+    o_flip,
 ]
+# USER INPUT END
 
 f = Findings()
-
-start_num = 4
-max_steps = 3
 derive(start_num, ops, max_steps, f)
-print('depth from', start_num, ':\n', f)
+print('depth from', start_num, ':\n', f.print_sols_with_min(min_steps, solution_predicate))
---
+++ operations.py
@@ -0,0 +1,51 @@
+
+class Operation:
+    def __init__(self, printable, func):
+        self.printable = printable;
+        self.func = func
+
+    def apply_to(self, x):
+        return self.func(x)
+
+    def __repr__(self):
+        return self.printable
+
+
+
+def map_digit(input, old, new):
+    return int(
+        ''.join(str(input).replace(str(old), str(new)))
+    )
+
+def o_map(x, y):
+    printable = str(x) + '=>' + str(y)
+    func = lambda q : map_digit(q, x, y)
+    return Operation(printable, func)
+
+def o_plus(x):
+    printable = '+' + str(x)
+    func = lambda q : q+x
+    return Operation(printable, func)
+
+def o_sub(x):
+    printable = '-' + str(x)
+    func = lambda q : q-x
+    return Operation(printable, func)
+
+def o_insert(x):
+    printable = '_' + str(x)
+    func = lambda q : q * 10 + x
+    return Operation(printable, func)
+
+def o_mult(x):
+    printable = '*' + str(x)
+    func = lambda q : q*x
+    return Operation(printable, func)
+
+def o_div(x):
+    printable = '/' + str(x)
+    func = lambda q : q/x
+    return Operation(printable, func)
+
+o_flip = Operation('+-', lambda x : -x)
+o_del = Operation('<<', lambda x : int(x/10))
